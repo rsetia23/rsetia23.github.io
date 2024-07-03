d4d3034b052128a91cff1672a7db68aa46560511
rsetia23 <rahulsetia2005@gmail.com>
2024-07-03 11:18:45 -0400

    Added some mobile styling

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 document.getElementById('light-mode-toggle').addEventListener('click', function() {
-    document.body.classList.toggle('light-mode');
+  document.body.classList.toggle('light-mode');
 
-    // Toggle icon
-    const icon = this.querySelector('i');
-    if (document.body.classList.contains('light-mode')) {
-        icon.classList.remove('fa-sun');
-        icon.classList.add('fa-moon');
-    } else {
-        icon.classList.remove('fa-moon');
-        icon.classList.add('fa-sun');
-    }
+  // Toggle icon
+  const icon = this.querySelector('i');
+  if (document.body.classList.contains('light-mode')) {
+      icon.classList.remove('fa-sun');
+      icon.classList.add('fa-moon');
+  } else {
+      icon.classList.remove('fa-moon');
+      icon.classList.add('fa-sun');
+  }
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-    const nameSpan = document.getElementById('typed-name');
-    const cursorSpan = document.getElementById('cursor');
-    const name = 'Rahul Setia'; // Replace with your name
-    
-    let charIndex = 0;
-    
-    function type() {
-      if (charIndex < name.length) {
-        nameSpan.textContent += name.charAt(charIndex);
-        charIndex++;
-        setTimeout(type, 150); // Adjust typing speed (milliseconds)
-      } else {
-        cursorSpan.style.display = 'inline-block'; // Show cursor after typing
-      }
+  const nameSpan = document.getElementById('typed-name');
+  const cursorSpan = document.getElementById('cursor');
+  const name = 'Rahul Setia'; // Replace with your name
+  
+  let charIndex = 0;
+  
+  function type() {
+    if (charIndex < name.length) {
+      nameSpan.textContent += name.charAt(charIndex);
+      charIndex++;
+      setTimeout(type, 150); // Adjust typing speed (milliseconds)
+    } else {
+      cursorSpan.style.display = 'inline-block'; // Show cursor after typing
     }
-    
-    type();
-  });
+  }
+  
+  type();
+});
 
 // Get the menu bar element
 const menuBar = document.querySelector('.menu-bar');
@@ -40,12 +40,17 @@ const offsetPosition = menuBar.offsetTop;
 
 // Function to handle scroll event
 function handleScroll() {
-  if (window.scrollY > offsetPosition) {
-    menuBar.classList.add('fixed');
-  } else {
-    menuBar.classList.remove('fixed');
-  }
+if (window.scrollY > offsetPosition) {
+  menuBar.classList.add('fixed');
+} else {
+  menuBar.classList.remove('fixed');
+}
 }
 
 // Listen for scroll events
 window.addEventListener('scroll', handleScroll);
+
+document.getElementById('hamburger').addEventListener('click', function() {
+  var menu = document.querySelector('.mobile-menu');
+  menu.classList.toggle('open');
+});
